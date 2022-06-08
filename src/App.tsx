2bb2ab7395f1cf4e12522cedd42213c335435e66
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2022-06-08 13:51:39 -0400

    Limpado projeto e adicionado função  listagem de fotos

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
+import { useState, useEffect } from 'react';
 import * as C from './App.styles';
+import * as Photos from './services/photos';
+import { Photo } from './types/Photo';
 
 function App() {
+
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState<Photo[]>([]);
+
+  useEffect(() => {
+    const getPhotos = async () => {
+      setLoading(true);
+      setPhotos(await Photos.getAll());
+      setLoading(false);
+    }
+    getPhotos();
+  }, [])
+
   return (
     <C.Container>
       <C.Area>
@@ -8,8 +24,26 @@ function App() {
 
         {/* Área de upload */}
 
-        {/* Lista de fotos */}
+        {loading && 
+          <C.ScreenWarning>
+            <div className='emoji'>✋</div>
+            <div>Carregando...</div>
+          </C.ScreenWarning>
+        }
 
+        {!loading && photos.length > 0 && 
+          <C.PhotoList>
+            {photos.map((item, index) => (
+              <div>{item.name}</div>
+            ))}
+          </C.PhotoList>
+        }
+        {!loading && photos.length === 0 &&
+          <C.ScreenWarning>
+          <div className='emoji'>😔</div>
+          <div>Não há fotos na galeria</div>
+        </C.ScreenWarning>
+        }
       </C.Area>
     </C.Container>
   );  
