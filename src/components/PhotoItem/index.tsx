8a6cff927cef0f88ab0b3159cad08cad849c05c9
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2022-06-09 22:51:31 -0400

    Adicionado função para excluir fotos da galeria

--- src/components/PhotoItem/index.tsx
+++ src/components/PhotoItem/index.tsx
@@ -1,15 +1,17 @@
-import * as C from './styles'; 
+import * as C from './styles';
 
 type Props = {
-  url: string;
-  name: string;
+    url: string;
+    name: string;
+    onDelete: (name: string) => void;
 }
 
-export const PhotoItem = ({ url, name } : Props) => {
-  return(
-    <C.Container>
-      <img src={url} alt={name} />
-      {name}
-    </C.Container>
-  );
+export const PhotoItem = ({ url, name, onDelete }: Props) => {
+    return (
+        <C.Container>
+            <img src={url} alt={name} />
+            {name}
+            <button onClick={()=>onDelete(name)}>Excluir</button>
+        </C.Container>
+    );
 }
